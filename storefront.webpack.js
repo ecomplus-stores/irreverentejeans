7d61030775aa08f048764cd8fdd22963c76b827e
Leonardo Matos <leomp120894@gmail.com>
2024-09-03 19:12:02 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,6 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
     }
   }
 })
